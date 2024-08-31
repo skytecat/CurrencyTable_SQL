074a00567ee2cb3fba22eaf83be969e370b69443
Alyona <107105936+skytecat@users.noreply.github.com>
2024-08-31 09:48:10 +0300

    Update currency_table_01.sql

--- currency_table_01.sql
+++ currency_table_01.sql
@@ -11,6 +11,7 @@ fut_cur AS (SELECT DISTINCT balance.user_id, balance.money, balance.currency_id,
 join_cur AS(SELECT COALESCE(p.user_id, f.user_id) AS user_id, COALESCE(p.money, f.money) AS money, COALESCE(p.currency_id, f.currency_id) AS currency_id, COALESCE(p.min, f.max) AS upd
             FROM past_cur p
                 FULL JOIN fut_cur f ON p.user_id = f.user_id AND p.money = f.money AND p.currency_id = f.currency_id AND p.updated = f.updated)
+
 SELECT DISTINCT COALESCE(u.name, 'not defined') AS name, COALESCE(u.lastname, 'not defined') AS lastname, c.name AS currency_name, money * rate_to_usd AS currency_in_usd
 FROM join_cur j
     LEFT JOIN "user" u ON user_id = u.id
